2e2e393529730b2259f0c704fc2be76464629980
Коноплев Тимур Евгеньевич <konoplevte@mai.ru>
2026-02-15 21:12:25 +0300

    feature: update cli message

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 def hello(name: str = "world") -> str:
     return f"Hello {name}."   # другое форматирование: без запятой и с точкой
+
+if __name__ == "__main__":
+    print("Running app...")
+    print(hello())
